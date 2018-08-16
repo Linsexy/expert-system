d200079ec540f5c98a4bf2661b121b7e61e244c5
Benoît Linsey <benoitlinsey20@gmail.com>
2018-08-16 20:40:57 +0200

    finished set up

---
+++ logic-expert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logic_expert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tom deska!");
+        }
+    }
+}
